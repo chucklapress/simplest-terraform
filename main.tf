09cfea37e72c420cfdd752c5842689931188220c
Charles LaPress <charleslapress@gmail.com>
2021-08-27 02:43:48 +0000

    ran apply and destroy local

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
-   required_version = ">= 0.12.26"
- }
- 
- output "terminal_output" {
-   value = "This is the result of the terraform apply"
- }
+  required_version = ">= 0.12.26"
+}
+
+output "terminal_output" {
+  value = "This is the result of the terraform apply"
+}
 
